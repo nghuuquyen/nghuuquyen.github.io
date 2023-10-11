3f3efbd279c4fc4a9e5e72f7255e59f85b7c0104
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2023-10-12 01:37:29 +0700

    Adding local storage to todo list

--- apps/todo-list/todo-list.js
+++ apps/todo-list/todo-list.js
@@ -1,7 +1,12 @@
 export default function toDoList() {
-    return {
+    const storageKey = 'todos'; // Key for storing todos in local storage
+
+    // Try to retrieve todos from local storage, or use an empty array if none is found
+    const savedTodos = JSON.parse(localStorage.getItem(storageKey)) || [];
+
+    const state = {
         newTodo: '',
-        todos: [],
+        todos: savedTodos,
         addToDo() {
             this.todos.push({
                 todo: this.newTodo,
@@ -9,23 +14,29 @@ export default function toDoList() {
             });
 
             this.newTodo = '';
+            this.saveToLocalStorage(); // Save to local storage when a new todo is added
         },
         toggleToDoCompleted(index) {
             this.todos[index].completed = !this.todos[index].completed;
+            this.saveToLocalStorage(); // Save to local storage when a todo's completion is toggled
         },
         deleteToDo(index) {
-            this.todos = this.todos.filter((todo, todoIndex) => {
-                return index !== todoIndex
-            })
+            this.todos = this.todos.filter((todo, todoIndex) => index !== todoIndex);
+            this.saveToLocalStorage(); // Save to local storage after a todo is deleted
         },
         numberOfToDosCompleted() {
             return this.todos.filter(todo => todo.completed).length;
         },
         toDoCount() {
-            return this.todos.length
+            return this.todos.length;
         },
         isLastToDo(index) {
-            return this.todos.length - 1 === index
+            return this.todos.length - 1 === index;
+        },
+        saveToLocalStorage() {
+            localStorage.setItem(storageKey, JSON.stringify(this.todos)); // Save the todos array to local storage
         }
     };
+
+    return state;
 }
